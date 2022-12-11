4894e657fc1220c3474d10fb87c2be38286bc1e8
tatayoyoo <rugbyyoann@hotmail.fr>
2022-12-11 21:41:01 +0100

    ADD : Step - Create a project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tower-of-heroes';
+  title = 'Tower of heroes';
 }
